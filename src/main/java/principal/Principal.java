aab92a6a74e3fc28adfdcf85268b15a30e773934
Davi Wolff <>
2025-05-12 10:37:07 -0300

    Início do projeto.

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,14 @@
+package principal;
+
+import vision.FrmCalculadora;
+
+/**
+ *
+ * @author daviw
+ */
+public class Principal {
+     public static void main(String[] args) {
+         FrmCalculadora janela = new FrmCalculadora();
+        janela.setVisible(true);
+     }
+}
